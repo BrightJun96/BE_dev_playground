679b59845e72b22f26d410448066ab026a093f3c
jjalseu <cjfwns966@gmail.com>
2025-02-11 00:09:41 +0900

    quiz 도메인 설계

--- src/domain/quiz/domain/quiz.domain.ts
+++ src/domain/quiz/domain/quiz.domain.ts
@@ -1,5 +1,57 @@
+import { Field } from "../../../shared/enum/field.enum";
+
 export class QuizDomain {
   id: number;
+  title: string;
+  content: string;
+  detailUrl: string;
+  field: Field;
   explanation: string;
   answer: number;
+  quizMetaData: object;
+  multipleChoices: object[];
+  createdAt: Date;
+  updatedAt: Date;
+  version: number;
+
+  constructor({
+    id,
+    title,
+    content,
+    detailUrl,
+    field,
+    explanation,
+    answer,
+    quizMetaData,
+    multipleChoices,
+    createdAt,
+    updatedAt,
+    version,
+  }: {
+    id: number;
+    title: string;
+    content: string;
+    detailUrl: string;
+    field: Field;
+    explanation: string;
+    answer: number;
+    quizMetaData: object;
+    multipleChoices: object[];
+    createdAt: Date;
+    updatedAt: Date;
+    version: number;
+  }) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.detailUrl = detailUrl;
+    this.field = field;
+    this.explanation = explanation;
+    this.answer = answer;
+    this.quizMetaData = quizMetaData;
+    this.multipleChoices = multipleChoices;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.version = version;
+  }
 }
