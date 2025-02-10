167924b4fa074237a57e32279411abf96cb991d6
jjalseu <cjfwns966@gmail.com>
2025-02-11 00:45:06 +0900

    typeorm-transaction adapter 설계

---
+++ src/shared/transaction/typeorm-transaction.adapter.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@nestjs/common";
+import { DataSource } from "typeorm";
+import { TransactionManagerPort } from "./port/transaction-manager.port";
+
+@Injectable()
+export class TypeormTransactionAdapter
+  implements TransactionManagerPort
+{
+  constructor(private readonly dataSource: DataSource) {}
+
+  async runInTransaction<T>(
+    fn: () => Promise<T>,
+  ): Promise<T> {
+    const qr = this.dataSource.createQueryRunner();
+    await qr.connect();
+    await qr.startTransaction();
+
+    try {
+      const result = await fn();
+
+      await qr.commitTransaction();
+      await qr.release();
+
+      return result;
+    } catch (e) {
+      await qr.rollbackTransaction();
+      await qr.release();
+      throw e;
+    }
+  }
+}
